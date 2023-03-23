0fbe362160da13627e6c8fa9159c7f46c829c5bc
kg-intec <116827748+kg-intec@users.noreply.github.com>
2023-03-23 07:17:52 +0100

    Update setup.py
    for version 3.10+

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
+    python_requires='>=3.10',
     name="mkdocs-doxygen-plugin",
 
     version="0.1.0",
